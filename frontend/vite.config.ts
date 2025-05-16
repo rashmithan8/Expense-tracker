cff2d722cafe903d42eeca4ccd06be70e4241787
rashmithan8 <rashmithan748@gmail.com>
2025-05-16 21:54:41 +0530

    second commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
+  base: "./",
+  build: {
+    outDir: "dist", // ✅ This is the output folder
+  },
+
+
   plugins: [
     react(),
     VitePWA({
